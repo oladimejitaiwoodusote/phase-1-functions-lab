7f731ceecf3658bc0c73eab28d0748a04c2cc260
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-02-20 19:30:14 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    return Math.abs(someValue - 42);
+}
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((destination - start) * 264);
+}
+
+function calculatesFarePrice(start, destination){
+    let price = distanceTravelledInFeet(start, destination);
+    if (price <= 400){
+        return 0;
+    }
+    else if (price > 400 && price < 2000){
+        return ((price - 400) * 2)/100;
+    }
+    else if (price > 2000 && price < 2500){
+        return (25);
+    }
+    else {
+        return "cannot travel that far";
+    }
+}
